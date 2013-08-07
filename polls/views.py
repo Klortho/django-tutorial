ed4b96757492a6872efea8db7590acb891a812a1
Chris Maloney <voldrani@gmail.com>
2013-08-06 20:29:00 -0400

    Adding polls template

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,15 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from polls.models import Poll
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
     output = ', '.join([p.question for p in latest_poll_list])
     return HttpResponse(output)
 
